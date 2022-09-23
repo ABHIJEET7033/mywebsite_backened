84cd1674354c8b6d3dacb73328e8a4970c6ed8c3
Abhijeet <2872000abhijeet@gmail.com>
2022-09-24 00:06:36 +0530

    final

---
+++ app.js
@@ -0,0 +1,46 @@
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const cookieparser=require('cookie-parser');
+dotenv.config();
+var corsOptions = {
+    origin: `${process.env.BASE_URL}`,
+    optionsSuccessStatus: 200,
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+require('./db/conn');
+app.use(express.json());
+app.use(cookieparser());
+app.use(require('./router/auth'));
+const User=require('./model/userSchema');
+
+const PORT=process.env.PORT;
+
+const middleware=(req,res,next)=>{
+           console.log('hello my middleware');
+           next();
+}
+app.get('/about', middleware, (req, res) => {
+    console.log(`Hello my About`);
+    res.send(`Hello About world from the server`);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`Hello Contact world from the server`);
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`Hello Login world from the server`);
+});
+
+app.get('/signup', (req, res) => {
+    res.send(`Hello Registration world from the server`);
+});
+
+app.listen(PORT, () => {
+    console.log(`server is runnig at port no ${PORT}`);
+})
